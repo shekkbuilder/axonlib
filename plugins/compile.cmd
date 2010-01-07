be2355a99b6c9257d8d141cab643c536fb993399
cern.th.skei <cern.th.skei@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-01-07 13:26:17 +0000

    windows compile scripts

---
+++ plugins/compile.cmd
@@ -0,0 +1,54 @@
+:: compile.cmd
+:: ----------------------
+@echo off
+:: check for file input
+if .%1==. goto nofile
+	:: check for 'cpp' extention
+	set tmpfile=%1
+	if not "%tmpfile:~-4%"==".cpp" goto nocpp
+		:: --------------------------
+		:: set path to mingw/bin
+		set mgwpath=
+		:: --------------------------	
+		:: set target
+		set infile=%1
+		set rstring=.cpp
+		call set target=%%infile:%rstring%=%%
+		set target=%target%.dll
+		:: --------------------------
+		echo.
+		echo * compiling windows binary for '%1'...
+		echo.
+		%mgwpath%mingw32-g++.exe -I../ax -I../../vstsdk24 -shared -mwindows -O3 -Os -fdata-sections -ffunction-sections %1 -s -o %target% -Wl,-gc-sections
+		%mgwpath%strip --strip-all %target%
+		:: optional upx
+		rem upx --best %target%
+		:: moving file
+		echo moving %target% to ../bin
+		move %target% ../bin
+		:: --------------------------
+		:: done
+		echo.
+		echo * done
+		echo ---------------------------------------------------------------------------
+		goto end
+:: ----------------------------------------------------------------------------
+:nocpp
+echo.
+echo ---------------------------------------------------------------------------
+echo @ axonlib compile script for windows
+echo @ usage: compile.cmd [file.cpp]
+echo ---------------------------------------------------------------------------
+echo ### input has to be a c++ file
+goto end
+:: ----------------------------------------------------------------------------
+:nofile
+echo.
+echo ---------------------------------------------------------------------------
+echo @ axonlib compile script for windows
+echo @ usage: compile.cmd [file.cpp]
+echo ---------------------------------------------------------------------------
+echo ### no input file entered
+goto end
+:: ----------------------------------------------------------------------------
+:end
