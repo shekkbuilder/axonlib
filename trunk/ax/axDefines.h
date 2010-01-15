d36c97906d1e1003de1dd23a7bb9927081d4b860
neolit123 <neolit123@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-01-15 22:13:35 +0000

    - axMath.h update

--- trunk/ax/axDefines.h
+++ trunk/ax/axDefines.h
@@ -8,30 +8,30 @@
   #define NULL 0
 #endif
 
-#define DENORM	10e-30f
-#define LOG2DB	8.68588963806f
-#define DB2LOG	0.11512925464f
+#define DENORM   10e-30f
+#define LOG2DB   8.68588963806f
+#define DB2LOG   0.11512925464f
 
-#define E				2.71828182845f
-#define LOG2E		1.44269504088f
-#define LOG10E	0.43429448190f
-#define LN2			0.69314718055f
-#define LN10		2.30258509299f
-#define PI			3.14159265358f
-#define PI2			6.28318530717f
-#define PI4			12.5663706143f
-#define PI_2		1.57079632679f
-#define PI_4		0.78539816339f
-#define D1_PI		0.31830988618f
-#define D2_PI		0.63661977236f
-#define SQRT2		1.41421356237f
-#define SQRT1_2	0.70710678118f
+#define E        2.71828182845f
+#define LOG2E    1.44269504088f
+#define LOG10E   0.43429448190f
+#define LN2      0.69314718055f
+#define LN10     2.30258509299f
+#define PI       3.14159265358f
+#define PI2      6.28318530717f
+#define PI4      12.5663706143f
+#define PI_2     1.57079632679f
+#define PI_4     0.78539816339f
+#define D1_PI    0.31830988618f
+#define D2_PI    0.63661977236f
+#define SQRT2    1.41421356237f
+#define SQRT1_2  0.70710678118f
 
-#define EPSILON 0.0001f
-#define inv360  (1.0f/360.0f)
-#define inv16   (1.0f/16.0f)
-#define inv127  (1.0f/127.0f)
-#define inv256  (1.0f/256.0f)
+#define EPSILON  0.0001f
+#define inv360   (1.0f/360.0f)
+#define inv16    (1.0f/16.0f)
+#define inv127   (1.0f/127.0f)
+#define inv256   (1.0f/256.0f)
 
 //#define i8    signed char
 //#define u8  unsigned char
--- trunk/plugins/compile.cmd
+++ trunk/plugins/compile.cmd
@@ -1,6 +1,10 @@
 :: compile.cmd
 :: ----------------------
 @echo off
+echo ---------------------------------------------------------------------------
+echo @ axonlib compile script for windows
+echo @ usage: compile.cmd [file.cpp]
+echo ---------------------------------------------------------------------------
 :: check for file input
 if .%1==. goto nofile
 	:: check for 'cpp' extention
@@ -15,22 +19,24 @@ if .%1==. goto nofile
 		set rstring=.cpp
 		call set target=%%infile:%rstring%=%%
 		set target=%target%.dll
+		set cmdpath=%~p0
+		rem call set tgtpath=%tgtpath:\=/%				 
 		:: --------------------------
-		if exist %target% del %target%;		
+		if exist %target% del %target%;
 		:: --------------------------
 		echo.
 		echo * compiling windows binary for '%1'...
-		echo.
-		%mgwpath%g++.exe -I../ax -I../../vstsdk24 -shared -mwindows -O3 -Os -fdata-sections -ffunction-sections -pedantic -fpermissive -Wl,-gc-sections %1 -s -o %target%
+		echo.		
+		%mgwpath%g++.exe -I%cmdpath%..\ax -I%cmdpath%..\..\vstsdk24 -shared -mwindows -msse -ffast-math -O3 -Os -fdata-sections -ffunction-sections -pedantic -fpermissive -s -Wl,-gc-sections .\%1 -o .\%target% %2 %3 %4
 		if exist %target% %mgwpath%strip --strip-all %target%
 		:: optional upx
 		rem upx --best %target%
 		:: moving file
 		echo.
-		if not exist ..\BIN md ..\bin		
+		if not exist %~p0..\BIN md %~p0..\bin
 		if not exist %target% echo # not compiled!		
-		if exist %target% echo moving %target% to ..\bin
-		if exist %target%	move %target% ..\bin
+		if exist %target% echo moving %target% to %~p0..\bin
+		if exist %target%	move %target% %~p0..\bin
 		:: --------------------------
 		:: done
 		echo.
@@ -40,19 +46,11 @@ if .%1==. goto nofile
 :: ----------------------------------------------------------------------------
 :nocpp
 echo.
-echo ---------------------------------------------------------------------------
-echo @ axonlib compile script for windows
-echo @ usage: compile.cmd [file.cpp]
-echo ---------------------------------------------------------------------------
 echo ### input has to be a c++ file
 goto end
 :: ----------------------------------------------------------------------------
 :nofile
 echo.
-echo ---------------------------------------------------------------------------
-echo @ axonlib compile script for windows
-echo @ usage: compile.cmd [file.cpp]
-echo ---------------------------------------------------------------------------
 echo ### no input file entered
 goto end
 :: ----------------------------------------------------------------------------
