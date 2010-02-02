1a60251767422cd1f2b96c6c0e616e21dfd04a3d
neolit123 <neolit123@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-02-02 23:14:18 +0000

    - add: documentation.html default in /docs/ (open doxy index using javascript) - axDebug.h - proposal for default debug methods - doxy -> some edits in: axMath, axMain, axUtils, axDebug

--- ax/axDebug.h
+++ ax/axDebug.h
@@ -17,38 +17,42 @@
  */
 
 /**
- * @file
- * \brief desc
+ * @file axDebug.h
+ * \brief debugger methods
+ *  
+ * if AX_DEBUG is defined, 'assert.h' and 'iostream' are included for debugging purposes. <br><br> 
+ * assert() examples:
+ * \code
+ * assert(var);   // if 'var' is false the compiler will trow an error and abort
+ * assert(num>5); // same as above but if 'num' is not greater than 5  
+ * \endcode
+ * warn() examples:
+ * \code
+ * warn("message"); // show a warning but does not break
+ * \endcode
+ * trace() examples:
+ * \code
+ * trace(var);              // trace a variable 
+ * trace("message");        // trace a message
+ * trace(var << "message"); // combined
+ * \endcode 
  */
 
-/**
- * \brief desc
- *
- * long desc
- *
- */
- 
 #ifndef axDebug_included
 #define axDebug_included
 //----------------------------------------------------------------------
 
 #ifdef AX_DEBUG
-
-  #include <stdio.h>
-  #define TRACE printf
-
+	#include <assert.h>
+	#include <iostream>
+	using namespace std;
+	#define warn(x) { printf("WARN | LINE: %i | %s\n", __LINE__, x); }
+	#define trace(x) { cout << "TRC | LINE: " << __LINE__ << " | " << x << endl; cout.flush(); }
 #else
-
   #define NDEBUG
-  static void do_nothing(...) {}
-  #define TRACE do_nothing
-
+  #define trace(x) ((void)0) // asserts.h uses this method to return 'void'
+  #define warn(x) ((void)0)
 #endif
-
-//----------
-
-#include <assert.h>
-
 
 //----------------------------------------------------------------------
 #endif
