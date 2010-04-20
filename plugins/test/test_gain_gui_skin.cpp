0f286adb6009aaeab3e090f904c674d89358b8be
cern.th.skei <cern.th.skei@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-04-20 00:57:15 +0000

    test_gain_gui at least works as win exe.. gui_skin has some problems with a memcpy in its prepare()

--- plugins/test/test_gain_gui_skin.cpp
+++ plugins/test/test_gain_gui_skin.cpp
@@ -43,6 +43,7 @@ class mySkin : public axSkinDefault
       }
     virtual void drawKnob(axCanvas* aCanvas, axRect aRect,  axString aName, axString aDisp, float aValue)
       {
+
         if (mKnobImage)
         {
           // bitmap
@@ -66,7 +67,10 @@ class mySkin : public axSkinDefault
             aCanvas->setTextColor(mTextColor);
             aCanvas->drawText(x+size+5,y,aRect.x2(),aRect.y2(),aDisp,ta_Left);
           }
-        }
+        } //knobimage
+        else
+
+        axSkinDefault::drawKnob(aCanvas,aRect,aName,aDisp,aValue);
       }
 };
 
@@ -130,20 +134,21 @@ class myPlugin : public axPlugin
         axEditor* editor = new axEditor(this,aContext,mEditorRect,AX_WIN_DEFAULT);
         axCanvas* canvas = editor->getCanvas();
         skin = new mySkin(canvas);
-        //--- decode & initialize bitmap
+        editor->setSkin(skin);
+//      //--- decode & initialize bitmap
         srf = editor->createSurface(32,32*65);
-        loader.decode((unsigned char*)knob32,knob32_size);
+          loader.decode((unsigned char*)knob32,knob32_size);
         axBitmap* bitmap = editor->createBitmap( loader.getWidth(), loader.getHeight() );
         bitmap->createBuffer( (char*)loader.getImage() );   // create bitmap buffer & copy data
         bitmap->convertRgbaBgra();                          // -> bgr.a
         bitmap->setBackground(128,128,128);                 // replace alpha
-        bitmap->prepare();                                  // prepare bitmap for blitting
+//        bitmap->prepare();                                  // prepare bitmap for blitting
         axCanvas* can = srf->getCanvas();
         can->drawBitmap(bitmap,0,0,0,0,32,32*65);           // upload to surface
         delete bitmap;
-        //---
+//        //---
         skin->setKnobImage(srf,65,32,32);
-        editor->setSkin(skin);
+//        editor->setSkin(skin);
         editor->appendWidget( wPanel = new wdgPanel(editor,NULL_RECT,wa_Client) );
         wPanel->appendWidget( w_Gain = new wdgKnob( editor,axRect(10,10,100,32),wa_None,"gain",0.75) );
         editor->connect(w_Gain,p_Gain);
