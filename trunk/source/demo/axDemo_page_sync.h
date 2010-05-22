9b315966740919b776401254b93424cad70db88b
neolit123 <neolit123@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-05-22 21:43:50 +0000

    * rename ./plugins -> ./source

---
+++ trunk/source/demo/axDemo_page_sync.h
@@ -0,0 +1,66 @@
+#ifndef axDemo_page_sync_included
+#define axDemo_page_sync_included
+//----------------------------------------------------------------------
+
+#include "wdg/wdgPanel.h"
+#include "axDemo_skin.h"
+
+//----------------------------------------------------------------------
+
+class axDemo_page_sync : public wdgPanel
+{
+  // let us access widgets, etc, directly
+  friend class axDemo;
+
+  private:
+    int   m_PlayState;
+    float m_SamplePos;
+    float m_SampleRate;
+    float m_BeatPos;
+    float m_Tempo;
+    char buf[256];
+
+  public:
+
+    axDemo_page_sync(axWidgetListener* aListener, axRect aRect, int aAlignment=wa_None)
+    : wdgPanel(aListener,aRect,aAlignment)
+      {
+      }
+
+    virtual ~axDemo_page_sync()
+      {
+      }
+
+    void update(int aPlayState,double aSamplePos,double aSampleRate,double aBeatPos,double aTempo)
+      {
+        m_PlayState   = aPlayState;
+        m_SamplePos   = aSamplePos;
+        m_SampleRate  = aSampleRate;
+        m_BeatPos     = aBeatPos;
+        m_Tempo       = aTempo;
+        if (isVisible())
+        {
+          //trace("redraw");
+          //trace("mGetPlayState " << aPlayState );
+          mListener->onRedraw(this);
+        }
+      }
+
+    virtual void doPaint(axCanvas* aCanvas, axRect aRect)
+      {
+        wdgPanel::doPaint(aCanvas,mRect);
+        axColor text = aCanvas->getColor(AX_WHITE);
+        int x = mRect.x;
+        int y = mRect.y;
+        aCanvas->setTextColor(text);
+        aCanvas->drawText(x+10,y+10,"play state");  aCanvas->drawText(x+80,y+10,axItoa(buf,m_PlayState));
+        aCanvas->drawText(x+10,y+25,"sample pos");  aCanvas->drawText(x+80,y+25,axFtoa(buf,m_SamplePos));
+        aCanvas->drawText(x+10,y+40,"sample rate"); aCanvas->drawText(x+80,y+40,axFtoa(buf,m_SampleRate));
+        aCanvas->drawText(x+10,y+55,"beat pos");    aCanvas->drawText(x+80,y+55,axFtoa(buf,m_BeatPos));
+        aCanvas->drawText(x+10,y+70,"tempo");       aCanvas->drawText(x+80,y+70,axFtoa(buf,m_Tempo));
+      }
+
+};
+
+//----------------------------------------------------------------------
+#endif
---
+++ trunk/source/demo/axDemo_page_system.h
@@ -0,0 +1,54 @@
+#ifndef axDemo_page_system_included
+#define axDemo_page_system_included
+//----------------------------------------------------------------------
+
+#include "wdg/wdgPanel.h"
+#include "axDemo_skin.h"
+#include "core/axUtils.h"
+
+//----------------------------------------------------------------------
+
+class axDemo_page_system : public wdgPanel
+{
+  // let us access widgets, etc, directly
+  friend class axDemo;
+
+  private:
+    axSystemInfo* mSystemInfo;
+
+  public:
+
+    axDemo_page_system(axWidgetListener* aListener, axRect aRect, int aAlignment=wa_None)
+    : wdgPanel(aListener,aRect,aAlignment)
+      {
+        mSystemInfo = NULL;
+      }
+
+    virtual ~axDemo_page_system()
+      {
+      }
+
+    void setup(axSystemInfo* aSystemInfo)
+      {
+        mSystemInfo = aSystemInfo;
+      }
+
+    virtual void doPaint(axCanvas* aCanvas, axRect aRect)
+      {
+        wdgPanel::doPaint(aCanvas,aRect);
+        if (mSystemInfo)
+        {
+          axColor text = aCanvas->getColor(AX_WHITE);
+          int x = mRect.x;
+          int y = mRect.y;
+          aCanvas->setTextColor(text);
+          aCanvas->drawText(x+10,y+10,"os");      aCanvas->drawText(x+80,y+10,mSystemInfo->os);
+          aCanvas->drawText(x+10,y+25,"cpuid");   aCanvas->drawText(x+80,y+25,mSystemInfo->cpuidstr);
+          aCanvas->drawText(x+10,y+40,"endian");  aCanvas->drawText(x+80,y+40,mSystemInfo->bigend?"big":"little");
+        }
+      }
+
+};
+
+//----------------------------------------------------------------------
+#endif
