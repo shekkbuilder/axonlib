b35deea819eb1de5cc29c356f28428a30d72153f
cern.th.skei <cern.th.skei@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-06-03 01:50:06 +0000

    pasted the axonlib license header text back into the files that were missing this

--- ax/axConfig.h
+++ ax/axConfig.h
@@ -1,3 +1,19 @@
+/*
+ * This file is part of Axonlib.
+ *
+ * Axonlib is free software: you can redistribute it and/or modify
+ * it under the terms of the Axonlib License, either version 1.0
+ * of the License, or (at your option) any later version.
+ *
+ * Axonlib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE_AX for more details.
+ *
+ * You should have received a copy of the Axonlib License
+ * If not, see <http://axonlib.googlecode.com/>.
+ */
+
 #ifndef axConfig_included
 #define axConfig_included
 //----------------------------------------------------------------------
@@ -12,6 +28,7 @@
 //#define AX_DEBUG_AUTO_STD       // axDebug.h
 //#define AX_DEBUG_AUTO_WIN       // axDebug.h
 //#define AX_DEBUG_MEM            // axMalloc.h
+//#define AX_DEBUG_NEW            //
 //#define AX_DEBUG_PNG            // picopng.h
 //#define AX_FORMAT_EXE           // axEditor.h
 //#define AX_FORMAT_VST           // axFormat.h
--- ax/format/axReaperExt.h
+++ ax/format/axReaperExt.h
@@ -1,3 +1,19 @@
+/*
+ * This file is part of Axonlib.
+ *
+ * Axonlib is free software: you can redistribute it and/or modify
+ * it under the terms of the Axonlib License, either version 1.0
+ * of the License, or (at your option) any later version.
+ *
+ * Axonlib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE_AX for more details.
+ *
+ * You should have received a copy of the Axonlib License
+ * If not, see <http://axonlib.googlecode.com/>.
+ */
+
 #ifndef axReaperExt_included
 #define axReaperExt_included
 //----------------------------------------------------------------------
@@ -40,21 +56,21 @@ class axReaperExt
           /*
             lii:
               '__may_alias' could be used on a type
-              (e.g. 'long_a'. see axDefines.h update ) 
-              
+              (e.g. 'long_a'. see axDefines.h update )
+
               or something in the lines of:
               union
-              { 
+              {
                 double (*mGetPlayPosition) ();
                 audioMasterCallback audioMaster;
               } u;
               u.mGetPlayPosition = mGetPlayPosition; // ?
               u.audioMaster = audioMaster(_PARAMS, (char*)"string",0.0);
-              
+
               //...
               //untested.
           */
-               
+
           // Reaper VST extensions
           *(long_a*)&mGetPlayPosition     = audioMaster(NULL,0xdeadbeef,0xdeadf00d,0,(char*)"GetPlayPosition",0.0);
           *(long_a*)&mGetPlayPosition2    = audioMaster(NULL,0xdeadbeef,0xdeadf00d,0,(char*)"GetPlayPosition2",0.0);
