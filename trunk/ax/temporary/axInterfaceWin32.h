587d894cf3ef1e64f7aa265e5956c8a47aa11acc
cern.th.skei <cern.th.skei@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-06-24 20:38:37 +0000

    * (new) win32 vst compiles now, lots of missing features, crashing, but we're improving, i guess...

--- trunk/ax/temporary/axInterfaceWin32.h
+++ trunk/ax/temporary/axInterfaceWin32.h
@@ -16,8 +16,8 @@ class axInterface
     int       getScreenWidth(void) { return 0; }
     int       getScreenHeight(void) { return 0; }
     int       getScreenDepth(void) { return 0; }
-    axWindow* createWindow(int aWidth, int aHeight) { return NULL; }
-    axWindow* createEmbeddedWindow(int aParent, int aWidth, int aHeight) { return NULL; }
+    //axWindow* createWindow(int aWidth, int aHeight) { return NULL; }
+    //axWindow* createEmbeddedWindow(int aParent, int aWidth, int aHeight) { return NULL; }
 };
 
 //----------------------------------------------------------------------
