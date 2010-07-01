1e9d6ce9cd06c2f0c6101f228d5660d8884c7be1
cern.th.skei <cern.th.skei@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-07-01 11:28:14 +0000

    switch, phase #1 (some directory structure changes first)

---
+++ trunk/ax/base/axInterfaceLinux.h
@@ -0,0 +1,62 @@
+#ifndef axInterfaceLinux_included
+#define axInterfaceLinux_included
+//----------------------------------------------------------------------
+/*
+  variations (flags, see axInterface.h)
+  - embedded/windowed
+  - buffered/unbuffered
+  - alpha/noalpha
+*/
+
+#include "core/axRect.h"
+#include "platform/axContext.h"
+#include "gui/axWindow.h"
+
+//void* threadProc(void* data);
+//void* timerProc(void* data);
+//void  eventProc(XEvent* ev);
+
+class axInterface
+{
+  private:
+    Display*  mDisplay;
+
+  public:
+
+    axInterface(axFormat* aFormat)
+      {
+        trace("axInterface linux");
+        XInitThreads();
+        mDisplay = XOpenDisplay(NULL);
+      }
+    virtual ~axInterface()
+      {
+        XCloseDisplay(mDisplay);
+      }
+
+    //----------
+
+    //int getScreenWidth(void)  { return 0; }
+    //int getScreenHeight(void) { return 0; }
+    //int getScreenDepth(void)  { return 0; }
+
+    //----------
+
+    virtual axWindow* createWindow(void* aParent, int aWidth, int aHeight)
+      {
+        trace("axInterfaceLinux.createWindow");
+        //int winflags = (if_Buffered|if_MsgDelete|if_MsgThread);
+        axContext context;
+        context.mDisplay = mDisplay; // all uses same connection, or one each?
+        if (aParent==NULL) context.mWindow = XDefaultRootWindow(mDisplay);
+        else context.mWindow = (Window)aParent;
+        context.mAudio = NULL;
+        axWindow* window = new axWindow(&context,axRect(0,0,aWidth,aHeight),AX_WIN_DEFAULT);
+        return window;
+        //return NULL;
+      }
+
+};
+
+//----------------------------------------------------------------------
+#endif
---
+++ trunk/ax/base/axInterfaceWin32.h
@@ -0,0 +1,79 @@
+#ifndef axInterfaceWin32_included
+#define axInterfaceWin32_included
+//----------------------------------------------------------------------
+/*
+  variations:
+  - embedded/windowed
+  - buffered/unbuffered
+  - alpha/noalpha
+*/
+
+#include <windows.h>
+
+#include "core/axRect.h"
+#include "platform/axContext.h"
+#include "gui/axWindow.h"
+
+//
+//  #define AX_CONTEXT_INIT(name)
+//    char* winname  = (char*)MAKE_NAME(name);
+
+class axInterface
+{
+  private:
+    axFormat* mFormat;
+    HINSTANCE mWinInstance;
+    WNDCLASS  mWinClass;
+    char      mWinClassName[256];
+
+  public:
+    axInterface(axFormat* aFormat)
+      {
+        wtrace("axInterfaceWin32");
+        mFormat = aFormat;
+        axPlatform* platform = aFormat->getPlatform();
+        mWinInstance = platform->getWinInstance();
+        //char* classname = mWinName.ptr();
+        axStrcpy( mWinClassName, mFormat->getName() );
+        axStrcat( mWinClassName, "_window" );
+        wtrace("mWinClassName: " << mWinClassName);
+        axMemset(&mWinClass,0,sizeof(mWinClass));
+        mWinClass.style          = CS_HREDRAW | CS_VREDRAW;
+        mWinClass.lpfnWndProc    = &eventProc;                    // !!!
+        mWinClass.hInstance      = mWinInstance;
+        mWinClass.lpszClassName  = mWinClassName;
+        //mWinClass.hCursor        = (HICON)mWinCursor;//LoadCursor(NULL, IDC_ARROW);
+        //HICON hIcon = LoadIcon(mWinInstance, "axicon");
+        //if (hIcon)mWinClass.hIcon = hIcon;
+        RegisterClass(&mWinClass);
+      }
+
+    virtual ~axInterface()
+      {
+        UnregisterClass(mWinClassName,mWinInstance);
+      }
+
+    //----------------------------------------------------------------------
+
+    //int getScreenWidth(void) { return 0; }
+    //int getScreenHeight(void) { return 0; }
+    //int getScreenDepth(void) { return 0; }
+
+    axWindow* createWindow(void* parent, int aWidth, int aHeight)
+      {
+        wtrace("axInterfaceWin32.createWindow");
+        axContext context;
+        context.mInstance     = mWinInstance;
+        context.mWinClassName = mWinClassName;;
+        context.mWindow       = (HWND)parent;
+        axWindow* window      = new axWindow(&context,axRect(0,0,aWidth,aHeight),AX_WIN_DEFAULT);
+        return window;
+        return NULL;
+      }
+
+};
+
+//----------------------------------------------------------------------
+#endif
+
+
