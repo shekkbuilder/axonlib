0f2a773051f866be908875aeae67dd594a0dc660
cern.th.skei <cern.th.skei@6d6a82ae-a189-11de-bc7b-8ff00adae793>
2010-07-06 22:08:28 +0000

    (missing new files)

---
+++ trunk/ax/core/axDebugLogFile.h
@@ -0,0 +1,27 @@
+#ifndef axDebugLogFile_included
+#define axDebugLogFile_included
+//----------------------------------------------------------------------
+
+//#include <io.h>
+//#include <stdio.h>
+//#include <sstream>
+
+class axDebugLogFile
+{
+  private:
+  public:
+    axDebugLogFile()
+      {
+      }
+    ~axDebugLogFile()
+      {
+      }
+};
+
+
+
+
+
+
+//----------------------------------------------------------------------
+#endif
